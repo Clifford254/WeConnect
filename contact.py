53232d733af9c430d6d465a8e59382196ea8f97c
jonnecliff <jonnecliff@gmail.com>
2018-03-01 12:49:02 +0300

    Update contact.py file #155614931

---
+++ contact.py
@@ -0,0 +1,17 @@
+class Contact():
+    #this is an empty dictionary to store the contacts
+    phonebook = {}
+    
+    def __init__(self, name=None, number=None):
+        """initializing class instance variables"""
+        self.name = name
+        self.number = number
+
+    def create(self, name, number):
+        pass
+    def edit(self, name, number):
+        pass
+    def view(self):
+        pass
+    def delete(self, name, number):
+        pass
