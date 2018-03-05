f9c54b2cd049a2eeac11776e11598ab8ff3a43f0
Clifford254 <jonnecliff@gmail.com>
2018-03-06 00:41:19 +0300

    set  up UserClass before  anything  and remove it after everything

--- tests/test_useraccount.py
+++ tests/test_useraccount.py
@@ -3,11 +3,22 @@ import unittest
 from app.useraccount import useraccount
 
 class UserAccountTestCases(unittest.TestCase)
+    """ Test for password length
+        Test for password mismatch when registering
+        Test for duplicate account
+        test login with no account
+        Test login with wrong password
+        Test login with correct email and password
     """
-    Test for password length
-    Test for password mismatch when registering
-    Test for duplicate account
-    test login with no account
-    Test login with wrong password
-    Test login with correct email and password
-    """
+
+    def setUp(self):
+        """set up UserClass before anything"""
+        
+        self.user = UserClass()
+
+    def tearDown(self):
+        """removing user class after everything"""
+
+        del self.user
+    
+
