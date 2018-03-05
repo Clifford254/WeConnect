2ab543f6f0cef854b527c27dfa46756bdc68b0b0
Clifford254 <jonnecliff@gmail.com>
2018-03-06 01:27:12 +0300

    test special character

--- tests/test_useraccount.py
+++ tests/test_useraccount.py
@@ -6,6 +6,9 @@ class UserAccountTestCases(unittest.TestCase)
     """ Test for password length
         Test for password mismatch when registering
         Test for duplicate account
+        Test for special character in username
+        Test for invalid email address
+        test for correct inputs
         test login with no account
         Test login with wrong password
         Test login with correct email and password
@@ -38,9 +41,18 @@ class UserAccountTestCases(unittest.TestCase)
 
     def test_duplicate_user(user):
         """checking for duplicate user"""
-        
+
         self.user.registerUser(
             "cliff", "cliff@gmail.com", "clifford", "clifford")
         msg = self.user.registerUser(
             "cliff", "cliff@gmail.com", "clifford", "clifford")
         self.assertIn("User already exists", msg)
+
+    def test_special_characters(self):
+        """checking if username contains special chsracters"""
+
+        msg = self.user.registerUser(
+            "cl!ff", "cliff@gmail.com", "clifford", "clifford") 
+        self.assertIn("No special character in username", msg)
+
+        
