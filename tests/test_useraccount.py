10723fb3c7db0893d74181b6ea69b3411834f217
Clifford254 <jonnecliff@gmail.com>
2018-03-06 21:55:09 +0300

    update useraccount and test_useraccount file

--- tests/test_useraccount.py
+++ tests/test_useraccount.py
@@ -27,69 +27,58 @@ class UserAccountTestCases(unittest.TestCase):
     def test_password_length(self):
         """checking for password length"""
 
-        msg = self.user.registerUser(
-            "cliff", "cliff@gmail.com", "cliffor", "cliffor")
-        self.assertEqual(
-            msg,"Your password should be atleat 8 characters")
+        msg = self.user.registerUser( "cliff", "cliff@gmail.com", "cliffor", "cliffor")
+        self.assertEqual(msg,"Your password should be atleast 8 characters")
 
     def test_password_mismatch(self):
         """checking for password mismatch"""
 
-        msg = self.user.registerUser(
-            "cliff", "cliff@gmail.com", "clifford", "clifforf")
+        msg = self.user.registerUser("cliff", "cliff@gmail.com", "clifford", "clifforf")
         self.assertEqual(msg, "Password mismatch")
 
-    def test_duplicate_user(user):
+    def test_duplicate_user(self):
         """checking for duplicate user"""
 
-        self.user.registerUser(
-            "cliff", "cliff@gmail.com", "clifford", "clifford")
-        msg = self.user.registerUser(
-            "cliff", "cliff@gmail.com", "clifford", "clifford")
-        self.assertIn("User already exists", msg)
+        self.user.registerUser("cliff", "cliff@gmail.com", "clifford", "clifford")
+        msg = self.user.registerUser("cliff", "cliff@gmail.com", "clifford", "clifford")
+        self.assertIn("User already exists.Please login", msg)
 
     def test_special_characters(self):
         """checking if username contains special chsracters"""
 
-        msg = self.user.registerUser(
-            "cl!ff", "cliff@gmail.com", "clifford", "clifford") 
-        self.assertIn("No special character in username", msg)
+        msg = self.user.registerUser("cl!ff", "cliff@gmail.com", "clifford", "clifford") 
+        self.assertIn("No special characters (. , ! space [] )", msg)
 
     def test_invalid_email(self):
         """check if email is valid"""
 
-        msg =(
-            "cliff", "cliffgmail.com", "clifford", "clifford")
+        msg =("cliff", "cliffgmail.com", "clifford", "clifford")
         self.assertEqual(msg,"Enter a valid email address")
     
     def test_correct_input(self):
         """check if inputs are correct on all fields"""
 
-        msg =(
-            "cliff", "cliff@gmail.com", "clifford", "clifford")
-        self.assertIn("Successfully registered",msg)
+        msg =("cliff", "cliff@gmail.com", "clifford", "clifford")
+        self.assertIn("Successfully registered. You can now login!",msg)
 
     def test_login_withoutaccount(self):
         """checks if user account exist"""
 
-        self.user.user_list = [
-            {'username':'cliff','email':'cliff@gmail.com','password':'clifford' }]
+        self.user.user_list = [{'username':'cliff','email':'cliff@gmail.com','password':'clifford' }]
         msg = self.user.login("clifford@gmail.com","clifford25")
         self.assertEqual(msg,"User does not exist, please register")
 
     def test_login_wrongpassword(self):
         """checks for wrong password during login"""
 
-        self.user.user_list = [
-            {'username':'cliff','email':'cliff@gmail.com','password':'clifford' }]
+        self.user.user_list = [{'username':'cliff','email':'cliff@gmail.com','password':'clifford' }]
         msg = self.user.login("cliff@gmail.com","clifford25")
         self.assertEqual(msg,"Wrong password")
 
     def test_correctlogin(self):
         """checks if user provide correct credentials"""
 
-        self.user.user_list = [
-            {'username':'cliff','email':'cliff@gmail.com','password':'clifford' }]
+        self.user.user_list = [{'username':'cliff','email':'cliff@gmail.com','password':'clifford' }]
         msg = self.user.login("cliff@gmail.com","clifford")
         self.assertIn(msg,"You can now Register a Business")
 
