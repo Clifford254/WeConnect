7abc912e7c323deae77ed759556ec9f209ef2707
Clifford254 <jonnecliff@gmail.com>
2018-03-06 07:33:47 +0300

    set up class to handle registration and login of users

---
+++ app/useraccount.py
@@ -0,0 +1,18 @@
+"""file to handle user account"""
+import re
+"""import regular expression"""
+
+class UserClass(object):
+    """class to handleregistration and login of user"""
+
+    def __init__(self):
+        """list containing users"""
+        self.user_list = ]
+
+    def registerUser(self, username, email, password,confirm_password)
+        """create registered users by adding to dictionary
+           Empty dict to hold each user"""
+        user_dict = {}
+    
+    def login(self,email,password):
+        
